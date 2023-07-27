30588edae6ddce9748072aaffe34d54809fad704
Xuan-369 <50647782+Xuan-369@users.noreply.github.com>
2023-07-27 15:40:05 +0800

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -1,6 +1,6 @@
 import type { AppInfo } from '@/types/app'
-export const APP_ID = '4a6fa0c8-629a-4ab9-88da-3fe2f4d45fbc'
-export const API_KEY = '<Web API Key From Dify>'
+export const APP_ID = ''
+export const API_KEY = ''
 export const API_URL = ''
 export const APP_INFO: AppInfo = {
   title: 'Text Generator APP',
@@ -15,3 +15,6 @@ export const API_PREFIX = '/api'
 export const LOCALE_COOKIE_NAME = 'locale'
 
 export const DEFAULT_VALUE_MAX_LEN = 48
+
+export const APP_ID = '4a6fa0c8-629a-4ab9-88da-3fe2f4d45fbc'
+export const API_KEY = '<Web API Key From Dify>'
